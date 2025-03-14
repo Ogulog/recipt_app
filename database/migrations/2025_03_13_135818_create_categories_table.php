9d241d5f7bdca613da571b11f14e4170c755dcc7
OGR <ogu.hide34317@gmail.com>
2025-03-14 23:09:23 +0900

    コメント追加

--- database/migrations/2025_03_13_135818_create_categories_table.php
+++ database/migrations/2025_03_13_135818_create_categories_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use log;
 
 return new class extends Migration
 {
@@ -11,6 +12,8 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Log::info("aaa");
+
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
